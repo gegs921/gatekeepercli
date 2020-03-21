cc384a930ffaa1014bbf9bee03d317c100fa49f1
talonbragg <w1tchhunt@protonmail.com>
2020-03-21 11:57:09 -0700

    setting up the main command

---
+++ bin/gatekeeper.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+require('yargs')
+  .scriptName("gk")
+  .usage('$0 <cmd> [args]')
+  .command('create [file]', 'main function', (yargs) => {
+    yargs.positional('file', {
+      type: 'string',
+      describe: 'file to iterate through'
+    })
+  }, function(argv) {
+    if(!argv.file) {
+      console.log('You need to enter a filename');
+      return 1;
+    }
+    else if(!fs.existsSync(argv.file)) {
+      console.log(`The file, ${argv.file}, does not exist.`);
+      return 1;
+    }
+    else {
+      // main code here
+    }
+  })
+  .help()
+  .argv();
